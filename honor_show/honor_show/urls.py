a62bec8dd70f36a63805c44fa92efc4fb43037d4
yasharzavary <zavaryyashar009@gmail.com>
2024-05-22 13:28:26 +0430

    bug fixed

--- honor_show/honor_show/urls.py
+++ honor_show/honor_show/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.main.urls')),
+    path('will_make', include('apps.will_make.urls'))
 ]
