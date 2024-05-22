a43dd2a5b2ab9528208edfa2915fbb0ebf2816b7
yasharzavary <zavaryyashar009@gmail.com>
2024-05-23 03:24:34 +0430

    image bug fixed

--- honor_show/honor_show/urls.py
+++ honor_show/honor_show/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('all_in_one', include('apps.all_in_one.urls')),
     path('contact', include('apps.contact.urls')),
     path('about', include('apps.about.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
