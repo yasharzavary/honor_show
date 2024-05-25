af909b1969a366f53ac437e6677aca0590c49b29
yasharzavary <zavaryyashar009@gmail.com>
2024-05-25 19:48:52 +0430

    models of one_image rewrite and updates

--- honor_show/apps/one_image/models.py
+++ honor_show/apps/one_image/models.py
@@ -1,6 +1,33 @@
 from django.db import models
 
+class Field(models.Model):
+    class Meta:
+        verbose_name = 'رشته'
+        verbose_name_plural = 'رشته ها'
+    field_name = models.CharField(max_length=45, verbose_name='نام رشته', blank=False, null=False)
+    
 
+class Person(models.Model):
+    class Meta:
+        verbose_name = 'دارنده افتخار'
+        verbose_name_plural = 'دارندگان افتخار'
+        
+    name = models.CharField(max_length=20, verbose_name='نام')
+    family = models.CharField(max_length=20, verbose_name='نام خانوداگی')
+    slug = models.SlugField()
+    age = models.IntegerField(blank=True, null=False, verbose_name='سن')
+    email = models.EmailField(max_length=100, blank=True, null=False, verbose_name='ایمیل')
+    DEGREES = [
+        ('under-diploma', 'زیر دیپلم'),
+        ('diploma', 'دیپلم'),
+        ('bachelor', 'کارشناسی'),
+        ('master', 'کارشناسی ارشد'),
+        ('Ph.d.', 'دکتری'),
+    ]
+    education_degree = models.CharField(choices=DEGREES, max_length=30, default='under-diploma', verbose_name='تحصیلات')
+    education_field = models.ForeignKey(Field, on_delete=models.CASCADE, null=False, blank=False, verbose_name='رشته تحصیلی')
+    
+    
 class Honor(models.Model):
     class Meta:
         verbose_name = 'افتخار'
@@ -15,15 +42,16 @@ class Honor(models.Model):
         ('university', 'دانشگاهی'),
         ('compition', 'مسابقات'),
         ('olympi', 'المپیاد'),
+        ('other', 'المپیاد'),
     ]
-    honor_field = models.CharField(choices=FIELDS, blank=False, max_length=20,  null=False, verbose_name='زمینه افتخار')
+    honor_field = models.CharField(choices=FIELDS, blank=False, max_length=20,  null=False, verbose_name = 'دسته افتخار')
+    owner = models.ForeignKey(Person, on_delete=models.CASCADE, blank=False, null=False, verbose_name = 'دارنده عنوان')
 
     def __str__(self):
         return f'name: {self.name}, image_path: {self.image_path}'
 
 
-class Person(models.Model):
-    class Meta:
-        verbose_name = 'دارنده افتخار'
-        verbose_name_plural = 'دارندگان افتخار'
-    name = models.CharField(max_length=20, verbose_name='نام')
+
+    
+    
+    
--- honor_show/apps/one_image/views.py
+++ honor_show/apps/one_image/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
+from .models import Honor
+from django.shortcuts import render
+from django.conf import settings
 
 def index(req):
     pass
 
 
 
-from django.shortcuts import render
-from apps.one_image.models import image
-from django.conf import settings
+
 
 
 def all(req):
     context = {
-        'trophies': image.objects.all(),
+        'trophies': Honor.objects.all(),
         'media_url': settings.MEDIA_URL,
     }
     return render(req, 'one_image/all.html', context)
