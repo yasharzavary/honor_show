66329779cccad898c58914654698446aa9a53d88
yasharzavary <zavaryyashar009@gmail.com>
2024-05-25 21:05:54 +0430

    bug fixed

--- honor_show/apps/one_image/admin.py
+++ honor_show/apps/one_image/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+import apps.one_image.models as model
 
-# Register your models here.
+@admin.register(model.Institute)
+class InstituteAdmin(admin.ModelAdmin):
+    list_display = ('name', 'build_date', 'work_goal')
+    search_fields = ('name', 'work_goal')
+    prepopulated_fields = {
+        'slug': ('name', 'build_date'),
+    }
+
+@admin.register(model.Field)
+class FieldAdmin(admin.ModelAdmin):
+    list_display = ('field_name',)
+    search_field = ('field_name',)
+
+
+@admin.register(model.Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'family', 'age', 'email', 'education_degree', 'education_field',)
+    search_field = ('name', 'family', 'education_field', 'education_degree',)
+    prepopulated_fields = {
+        'slug': ('name', 'family',)
+    }
+    
+    
+@admin.register(model.Honor)
+class HonorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'honor_field', 'owner',)
+    prepopulated_fields = {
+        'slug': ('name',)
+    }
+    search_fields = ('name', 'owner', 'honor_field',)
+    
--- honor_show/apps/one_image/apps.py
+++ honor_show/apps/one_image/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class OneImageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.one_image'
+    verbose_name = 'دارندگان افتخارات'
