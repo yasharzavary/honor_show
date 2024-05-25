724109496b816d95046322c046520885909fbed7
yasharzavary <zavaryyashar009@gmail.com>
2024-05-25 19:23:23 +0430

    honor model class added to the proejct

--- honor_show/apps/one_image/models.py
+++ honor_show/apps/one_image/models.py
@@ -1,17 +1,29 @@
 from django.db import models
 
 
-
-
-class honor(models.Model):
+class Honor(models.Model):
     class Meta:
         verbose_name = 'افتخار'
         verbose_name_plural = 'افتخارات'
         
-    name = models.CharField(max_length=30, default='trhopy', verbose_name='نام')
+    name = models.CharField(max_length=30, default='trhopy', verbose_name='عنوان')
     image_path = models.CharField(max_length=20, default='no_photo.jpg')
     desc = models.TextField(verbose_name='توضیحات', blank=True)
     when_get = models.DateTimeField(auto_now_add=True, blank=False, null=False, verbose_name='تاریخ کسب عنوان')
-    
+    slug = models.SlugField()
+    FIELDS = [
+        ('university', 'دانشگاهی'),
+        ('compition', 'مسابقات'),
+        ('olympi', 'المپیاد'),
+    ]
+    honor_field = models.CharField(choices=FIELDS, blank=False, max_length=20,  null=False, verbose_name='زمینه افتخار')
+
     def __str__(self):
         return f'name: {self.name}, image_path: {self.image_path}'
+
+
+class Person(models.Model):
+    class Meta:
+        verbose_name = 'دارنده افتخار'
+        verbose_name_plural = 'دارندگان افتخار'
+    name = models.CharField(max_length=20, verbose_name='نام')
