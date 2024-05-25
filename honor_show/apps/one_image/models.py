356177bd98ad4f9e7b2fb38c6385252a32189952
yasharzavary <zavaryyashar009@gmail.com>
2024-05-25 19:17:22 +0430

    image model deleted for better rewriting

--- honor_show/apps/one_image/models.py
+++ honor_show/apps/one_image/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
-class image(models.Model):
-    name = models.CharField(max_length=30)
-    image_path = models.CharField(max_length=20)
-    desc = models.TextField()
-    where_get = models.IntegerField(default=0)
 
 
+
+class honor(models.Model):
+    class Meta:
+        verbose_name = 'افتخار'
+        verbose_name_plural = 'افتخارات'
+        
+    name = models.CharField(max_length=30, default='trhopy', verbose_name='نام')
+    image_path = models.CharField(max_length=20, default='no_photo.jpg')
+    desc = models.TextField(verbose_name='توضیحات', blank=True)
+    when_get = models.DateTimeField(auto_now_add=True, blank=False, null=False, verbose_name='تاریخ کسب عنوان')
+    
     def __str__(self):
         return f'name: {self.name}, image_path: {self.image_path}'
